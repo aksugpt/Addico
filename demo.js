b7bfa70f7d6eb37c9e37a77b03a4b1a951fd5a56
akshay <akshay.gupta@gmail.com>
2021-10-28 16:33:04 +0545

    website created

---
+++ demo.js
@@ -0,0 +1,114 @@
+function validate(){
+    var name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    
+    error_message.style.padding = "10px";
+    
+    var text;
+    if(name.length < 5){
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(subject.length < 10){
+      text = "Please Enter Correct Subject";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(isNaN(phone) || phone.length != 10){
+      text = "Please Enter valid Phone Number";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+      text = "Please Enter valid Email";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length <= 40){
+      text = "Please Enter More Than 40 Characters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+  }
+
+//   feedback form code starts here
+let form = document.getElementById('form');
+let firstname = document.getElementById('firstname');
+let lastname = document.getElementById('lastname');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let textarea = document.getElementById('textarea');
+
+//Show input error messages
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//show success colour
+function showSucces(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())) {
+        showSucces(input)
+    }else {
+        showError(input,'Email is not invalid');
+    }
+}
+
+
+//checkRequired fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input,`${getFieldName(input)} is required`)
+        }else {
+            showSucces(input);
+        }
+    });
+}
+
+
+//check input Length
+function checkLength(input, min ,max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    }else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be les than ${max} characters`);
+    }else {
+        showSucces(input);
+    }
+}
+
+//get FieldName
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+//Event Listeners
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    checkRequired([firstname,lastname, email, phone, textarea]);
+    checkLength(firstname,3,15);
+    checkLength(lastname,3,15);
+    checkEmail(email);  
+    checkPhone(phone);  
+    checktextarea(textarea);  
+});
+//   feedback form code ends here
